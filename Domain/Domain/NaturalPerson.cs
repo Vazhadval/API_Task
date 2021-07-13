05e94ff160cfb765b0f29bc9b4b6705c64bce174
Vazhadval <vazhadvalishvili@gmail.com>
2021-07-13 12:30:41 +0400

    done

--- Domain/Domain/NaturalPerson.cs
+++ Domain/Domain/NaturalPerson.cs
@@ -12,23 +12,19 @@ namespace NaturalPersonAPI.Domain
     public class NaturalPerson
     {
         [Key]
-        [DoNotPatch]
         public long Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Gender { get; set; }
-        [DoNotPatch]
         public string PersonalNumber { get; set; }
         public DateTime BirthDate { get; set; }
         public int CityId { get; set; }
         [ForeignKey(nameof(CityId))]
-        [DoNotPatch]
         public City City { get; set; }
         public virtual List<PhoneNumber> PhoneNumbers { get; set; }
         public string Photo { get; set; }
 
         [NotMapped]
-        [DoNotPatch]
         public List<NaturalPerson> RelatedPeople { get; set; }
 
     }
--- NaturalPersonAPI/Helper/ExtensionMethods.cs
+++ NaturalPersonAPI/Helper/ExtensionMethods.cs
@@ -218,21 +218,5 @@ namespace NaturalPersonAPI.Helper
         {
             return builder.UseMiddleware<ErrorLoggingMiddleware>();
         }
-
-        public static void CheckForNotUpdatableProperties<T>(this Microsoft.AspNetCore.JsonPatch.JsonPatchDocument<T> document) where T : class
-        {
-            for (int i = document.Operations.Count - 1; i >= 0; i--)
-            {
-                string pathPropertyName = document.Operations[i].path.Split("/", StringSplitOptions.RemoveEmptyEntries).FirstOrDefault();
-
-                if (typeof(T).GetProperties().Where(p => p.IsDefined(typeof(DoNotPatchAttribute), true) && string.Equals(p.Name, pathPropertyName, StringComparison.CurrentCultureIgnoreCase)).Any())
-                {
-                    // remove
-                    document.Operations.RemoveAt(i);
-
-                    //todo: log removal
-                }
-            }
-        }
     }
 }
